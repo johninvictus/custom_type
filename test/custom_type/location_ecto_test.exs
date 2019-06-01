a88795627d408a72bb432061bd3a5875f927324c
johninvictus <invictusnitude@gmail.com>
2019-06-01 17:00:22 +0300

    Tested cast function and passed

--- test/custom_type/location_ecto_test.exs
+++ test/custom_type/location_ecto_test.exs
@@ -8,7 +8,7 @@ defmodule CustomType.Location.EctoTest do
   describe "location_ecto" do
     @location_map {44.968046, -94.420307}
 
-    test "cast function}" do
+    test "cast function" do
       assert {:ok, %Location{} = location} = Location.Ecto.cast(@location_map)
       assert {:ok, %Location{}} = Location.Ecto.cast(location)
       assert Location.Ecto.cast("location") == :error
